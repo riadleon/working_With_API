d06c70c23faaf6475fe15a859f9d4381c5c42dde
riadshalahinleon <riadshalahinleon@gmail.com>
2022-09-02 14:43:28 +0600

    kanye random rest

---
+++ js/countries.js
@@ -0,0 +1,44 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data))
+}
+
+const displayCountries = countries => {
+    // for (const country of countries)
+    //     console.log(country);
+    const countriesConatiner = document.getElementById('countries-container');
+    countries.forEach(country => {
+        // console.log(country);
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('country');
+        countryDiv.innerHTML = `
+        
+        <h3>Name  : ${country.name.common}</h3>
+        <p>Capital : ${country.capital ? country.capital[0] : 'No capital'}</p>
+        <button onclick="loadCountryDetail('${country.cca2}')">Details</button>
+        
+        
+        `;
+        countriesConatiner.appendChild(countryDiv);
+    })
+}
+
+    const loadCountryDetail =(code)=>{
+        const url = `https://restcountries.com/v3.1/alpha/${code}`
+        // console.log('get country details' , code);
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetail(data[0]))
+    }
+
+
+    const displayCountryDetail = country =>{
+        const countryDetail = document.getElementById('country-detail');
+        countryDetail.innerHTML = `
+        <h2>Name  : ${country.name.common}</h2>
+        <img src=" ${country.flags.png}"></img>
+        `
+    }
+
+loadCountries();
